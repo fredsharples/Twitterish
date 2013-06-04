37f68cfb3667f53ca314024393450c157dab2d97
Fred Sharples <fred@orangedesign.com>
2013-06-03 19:43:37 -0700

    Initial Commit

---
+++ Twitterish/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitterish
+//
+//  Created by Fred Sharples on 6/3/13.
+//  Copyright (c) 2013 Fred Sharples. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitterish/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Twitterish
+//
+//  Created by Fred Sharples on 6/3/13.
+//  Copyright (c) 2013 Fred Sharples. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
